829e0be40eef410d46a2afc0f947ebc3de3025d9
lislovelly <lislovelly@gmail.com>
2024-01-04 12:22:36 -0300

    update

--- aulas html/pokedex/assets/js/main.js
+++ aulas html/pokedex/assets/js/main.js
@@ -1,3 +1,33 @@
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+function convertPokemonToLi(pokemon){
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+                <img src="/assets/Bulbasaur.png" alt="${pokemon.name}">
+            </div>               
+        </li>`
+}
+
+const pokemonList = document.getElementById("pokemonList")
+
+pokeApi.getPokemons().then((pokemons = []) => { 
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+})
 
 
-console.log('Sucesso!');
+
+    
+
+
+
+
+
