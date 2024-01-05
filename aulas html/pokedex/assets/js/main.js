7603f9dd734b90b888bb75d559c6ea95a4c2b698
lislovelly <lislovelly@gmail.com>
2024-01-05 10:47:15 -0300

    update

--- aulas html/pokedex/assets/js/main.js
+++ aulas html/pokedex/assets/js/main.js
@@ -1,23 +1,27 @@
 
 function convertPokemonToLi(pokemon){
     return `
-        <li class="pokemon">
-            <span class="number">#001</span>
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
                 <ol class="types">
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
+                    ${pokemon.types.map((type) => `<li class="type"${type}>${type}</li>`).join('')}
                 </ol>
-                <img src="/assets/Bulbasaur.png" alt="${pokemon.name}">
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
             </div>               
         </li>`
 }
 
 const pokemonList = document.getElementById("pokemonList")
-
+//pokeApi.getPokemons requisição http para buscar uma lista de pokemon
+//then((pokemons = []) recebemos a lista de pokemon
+//pokemons.map(convertPokemonToLi) transformanmos esses pokemons em uma lista html
+//join('') concatenamos essa lista sem nehhum separador
 pokeApi.getPokemons().then((pokemons = []) => { 
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml
 })
 
 
--- aulas html/pokedex/assets/js/poke-api.js
+++ aulas html/pokedex/assets/js/poke-api.js
@@ -1,9 +1,52 @@
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+function convertPokeApoDetalToPokemon(pokeDetail){
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+    .then((Response) => Response.json())
+    .then(convertPokeApoDetalToPokemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+//return fetch(url) requisição para trazer a lista de pokemons 
+//converteu essa lista pra json
+//e transformar essa lista de pokemons em uma busca por detalhes
+//de promessas dos detalhes dos pokemons
+//then((detailRequests) requisiçoes de detalhes, 
+//Promise.all(detailRequests)) lista de promessas esperando ser resolvida
     return fetch(url)
     .then((Response) => Response.json())
     .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.error(error))
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonsDetails) => pokemonsDetails)
+        
 }
+
+//Promise.all esperando que todas essas promessas terminem
+//.then((results) quando elas terminarem vai cahmar o then com os resultados
+Promise.all([
+    fetch('https://pokeapi.co/api/v2/pokemon/1'),
+    fetch('https://pokeapi.co/api/v2/pokemon/2'),
+    fetch('https://pokeapi.co/api/v2/pokemon/3'),
+    fetch('https://pokeapi.co/api/v2/pokemon/4'),
+    fetch('https://pokeapi.co/api/v2/pokemon/5'),
+]).then((results) => {
+    console.log(results)
+})
