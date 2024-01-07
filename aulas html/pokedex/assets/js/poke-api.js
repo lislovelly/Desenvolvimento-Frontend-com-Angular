8d0fff44b4d31da62f99dcbdffb001e4cbff4350
Elisangela Mendonça <lislovelly@gmail.com>
2024-01-07 10:18:27 -0300

    Update poke-api.js

--- aulas html/pokedex/assets/js/poke-api.js
+++ aulas html/pokedex/assets/js/poke-api.js
@@ -1,12 +1,14 @@
+
 const pokeApi = {}
 
-function convertPokeApoDetalToPokemon(pokeDetail){
+function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.order
+    pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
 
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
+
     pokemon.types = types
     pokemon.type = type
 
@@ -15,38 +17,19 @@ function convertPokeApoDetalToPokemon(pokeDetail){
     return pokemon
 }
 
-
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
-    .then((Response) => Response.json())
-    .then(convertPokeApoDetalToPokemon)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-//return fetch(url) requisição para trazer a lista de pokemons 
-//converteu essa lista pra json
-//e transformar essa lista de pokemons em uma busca por detalhes
-//de promessas dos detalhes dos pokemons
-//then((detailRequests) requisiçoes de detalhes, 
-//Promise.all(detailRequests)) lista de promessas esperando ser resolvida
-    return fetch(url)
-    .then((Response) => Response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-    .then((detailRequests) => Promise.all(detailRequests))
-    .then((pokemonsDetails) => pokemonsDetails)
-        
-}
 
-//Promise.all esperando que todas essas promessas terminem
-//.then((results) quando elas terminarem vai cahmar o then com os resultados
-Promise.all([
-    fetch('https://pokeapi.co/api/v2/pokemon/1'),
-    fetch('https://pokeapi.co/api/v2/pokemon/2'),
-    fetch('https://pokeapi.co/api/v2/pokemon/3'),
-    fetch('https://pokeapi.co/api/v2/pokemon/4'),
-    fetch('https://pokeapi.co/api/v2/pokemon/5'),
-]).then((results) => {
-    console.log(results)
-})
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+}
